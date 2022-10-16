332ea8c9890e25279d959f3cf7d31c009154f293
Zack Fiddler <zack.fiddler@me.com>
2022-10-15 20:56:12 -0700

    add working buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log('hello world')
+const lowerBet = document.getElementById('lower-bet')
+const raiseBet = document.getElementById('raise-bet')
+const maxBet = document.getElementById('max-bet')
+const spinWheel = document.getElementById('spin-slot')
+console.log(raiseBet)
+console.log(lowerBet)
+console.log(maxBet)
+console.log(spinWheel)
+lowerBet.addEventListener('click', onLowerClicked)
+raiseBet.addEventListener('click', onRaiseClicked)
+maxBet.addEventListener('click', onMaxClicked)
+spinWheel.addEventListener('click', onSpinClicked)
+
+
+function onLowerClicked() {
+console.log('lower clicked')
+}
+function onRaiseClicked() {
+    console.log('raise clicked')
+}
+function onMaxClicked() {
+    console.log('max clicked')
+}
+function onSpinClicked() {
+    console.log('spin to win')
+}
+
