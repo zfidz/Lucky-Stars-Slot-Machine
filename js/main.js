1d16753a2d0bf78def4624363d610c067c73081e
Zack Fiddler <zack.fiddler@me.com>
2023-02-21 11:45:41 -0800

    refactor spin function, add animation

--- js/main.js
+++ js/main.js
@@ -50,87 +50,136 @@ function onMaxClicked() {
 }
 
 function onSpinClicked() {
-	symbolOne.style.backgroundImage = ""
-	symbolTwo.style.backgroundImage = ""
-	symbolThree.style.backgroundImage = ""
+	const symbols = [symbolOne, symbolTwo, symbolThree];
+	symbols.forEach(symbol => symbol.style.backgroundImage = "");
+
+	const noti = document.getElementById('notification');
+	const numbers = [getRandomNumber(), getRandomNumber(), getRandomNumber()];
+	const images = ["Astronaut", "Rocket", "Saturn"];
+
 	if (currentBet === 0) {
-		document.getElementById('notification').innerHTML = 'Please Enter Bet'
-		setTimeout(function() {
-			document.getElementById('notification').innerHTML = ''
-		}, 1500)
-	} else {
-		const noti = document.getElementById('notification')
-		let num1 = getRandomNumber()
-		let num2 = getRandomNumber()
-		let num3 = getRandomNumber()
-		setTimeout(function() {
-			if (num1 === 1) {
-				symbolOne.style.backgroundImage = "url('images/Astronaut.png')"
-			} else if (num1 === 2) {
-				symbolOne.style.backgroundImage = "url('images/Rocket.png')"
-			} else if (num1 === 3) {
-				symbolOne.style.backgroundImage = "url('images/Saturn.png')"
-			}
-		}, 750)
-		setTimeout(function() {
-			if (num2 === 1) {
-				symbolTwo.style.backgroundImage = "url('images/Astronaut.png')"
-			} else if (num2 === 2) {
-				symbolTwo.style.backgroundImage = "url('images/Rocket.png')"
-			} else if (num2 === 3) {
-				symbolTwo.style.backgroundImage = "url('images/Saturn.png')"
-			}
-		}, 1500)
-		setTimeout(function() {
-			if (num3 === 1) {
-				symbolThree.style.backgroundImage = "url('images/Astronaut.png')"
-			} else if (num3 === 2) {
-				symbolThree.style.backgroundImage = "url('images/Rocket.png')"
-			} else if (num3 === 3) {
-				symbolThree.style.backgroundImage = "url('images/Saturn.png')"
-			}
-		}, 2250)
-		setTimeout(function() {
-			if (num1 === 3 && num1 === num2 && num1 === num3) {
-				currentCredits = currentBet * 10 + currentCredits
-				lastWin = currentBet * 10
-				currentBet = 0
-				noti.innerHTML = 'Jackpot!!!'
-				setTimeout(function() {
-					noti.innerHTML = ''
-				}, 1500)
-			} else if (num1 === 2 && num1 === num2 && num1 === num3) {
-				currentCredits = currentBet * 4 + currentCredits
-				lastWin = currentBet * 6
-				currentBet = 0
-				noti.innerHTML = 'Winner!!'
-				setTimeout(function() {
-					noti.innerHTML = ''
-				}, 1500)
-			} else if (num1 === 1 && num1 === num2 && num1 === num3) {
-				currentCredits = currentBet * 4 + currentCredits
-				lastWin = currentBet * 4
-				currentBet = 0
-				noti.innerHTML = 'You Win!'
-				setTimeout(function() {
-					noti.innerHTML = ''
-				}, 1500)
-			} else {
-				if (currentCredits > 0) {
-					noti.innerHTML = 'Try Again!'
-					currentBet = 0
-					setTimeout(function() {
-						noti.innerHTML = ''
-					}, 1500)
-				} else {
-					currentBet = 0
-					gameOver()
-				}
-			}
-			updateValues()
-		}, 2500)
+	  noti.innerHTML = 'Please Enter Bet'
+	  return setTimeout(() => document.getElementById('notification').innerHTML = '', 1500);
 	}
-}
+
+	for (let i = 0; i < 3; i++) {
+	  let j = 0;
+	  const interval = setInterval(() => {
+		j++;
+		symbols[i].style.backgroundImage = `url('images/${images[j % 3]}.png')`;
+		if (j >= 30) {
+		  clearInterval(interval);
+		  symbols[i].style.backgroundImage = `url('images/${images[numbers[i]-1]}.png')`;
+		}
+	  }, 25 * (i+1));
+	}
+
+	setTimeout(() => {
+	  const winCombos = [[1,1,1],[2,2,2],[3,3,3]];
+	  const winAmts = [4, 6, 10];
+
+	  const index = winCombos.findIndex(combo => combo.every((num, i) => num === numbers[i]));
+	  if (index !== -1) {
+		const winAmt = currentBet * winAmts[index]
+		currentCredits += winAmt;
+		lastWin = winAmt;
+		currentBet = 0;
+		noti.innerHTML = index === 8 ? "Jackpot!!!" : "Winner!!";
+	  } else {
+		if (currentCredits <= 0 && currentBet === 0) {
+			gameOver();
+		  } else {
+			noti.innerHTML = "Try Again!";
+		  }
+		  currentBet = 0;
+		}
+	  updateValues();
+	  setTimeout(() => noti.innerHTML= "", 1500);
+	}, 2000);
+  }
+
+// function onSpinClicked() {
+// 	symbolOne.style.backgroundImage = ""
+// 	symbolTwo.style.backgroundImage = ""
+// 	symbolThree.style.backgroundImage = ""
+// 	if (currentBet === 0) {
+// 		document.getElementById('notification').innerHTML = 'Please Enter Bet'
+// 		setTimeout(function() {
+// 			document.getElementById('notification').innerHTML = ''
+// 		}, 1500)
+// 	} else {
+// 		const noti = document.getElementById('notification')
+// 		let num1 = getRandomNumber()
+// 		let num2 = getRandomNumber()
+// 		let num3 = getRandomNumber()
+// 		setTimeout(function() {
+// 			if (num1 === 1) {
+// 				symbolOne.style.backgroundImage = "url('images/Astronaut.png')"
+// 			} else if (num1 === 2) {
+// 				symbolOne.style.backgroundImage = "url('images/Rocket.png')"
+// 			} else if (num1 === 3) {
+// 				symbolOne.style.backgroundImage = "url('images/Saturn.png')"
+// 			}
+// 		}, 750)
+// 		setTimeout(function() {
+// 			if (num2 === 1) {
+// 				symbolTwo.style.backgroundImage = "url('images/Astronaut.png')"
+// 			} else if (num2 === 2) {
+// 				symbolTwo.style.backgroundImage = "url('images/Rocket.png')"
+// 			} else if (num2 === 3) {
+// 				symbolTwo.style.backgroundImage = "url('images/Saturn.png')"
+// 			}
+// 		}, 1500)
+// 		setTimeout(function() {
+// 			if (num3 === 1) {
+// 				symbolThree.style.backgroundImage = "url('images/Astronaut.png')"
+// 			} else if (num3 === 2) {
+// 				symbolThree.style.backgroundImage = "url('images/Rocket.png')"
+// 			} else if (num3 === 3) {
+// 				symbolThree.style.backgroundImage = "url('images/Saturn.png')"
+// 			}
+// 		}, 2250)
+// 		setTimeout(function() {
+// 			if (num1 === 3 && num1 === num2 && num1 === num3) {
+// 				currentCredits = currentBet * 10 + currentCredits
+// 				lastWin = currentBet * 10
+// 				currentBet = 0
+// 				noti.innerHTML = 'Jackpot!!!'
+// 				setTimeout(function() {
+// 					noti.innerHTML = ''
+// 				}, 1500)
+// 			} else if (num1 === 2 && num1 === num2 && num1 === num3) {
+// 				currentCredits = currentBet * 4 + currentCredits
+// 				lastWin = currentBet * 6
+// 				currentBet = 0
+// 				noti.innerHTML = 'Winner!!'
+// 				setTimeout(function() {
+// 					noti.innerHTML = ''
+// 				}, 1500)
+// 			} else if (num1 === 1 && num1 === num2 && num1 === num3) {
+// 				currentCredits = currentBet * 4 + currentCredits
+// 				lastWin = currentBet * 4
+// 				currentBet = 0
+// 				noti.innerHTML = 'You Win!'
+// 				setTimeout(function() {
+// 					noti.innerHTML = ''
+// 				}, 1500)
+// 			} else {
+// 				if (currentCredits > 0) {
+// 					noti.innerHTML = 'Try Again!'
+// 					currentBet = 0
+// 					setTimeout(function() {
+// 						noti.innerHTML = ''
+// 					}, 1500)
+// 				} else {
+// 					currentBet = 0
+// 					gameOver()
+// 				}
+// 			}
+// 			updateValues()
+// 		}, 2500)
+// 	}
+// }
 
 function getRandomNumber() {
 	return Math.floor(Math.random() * 3) + 1
